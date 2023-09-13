ce28ac3ab6872bc16f1f6a2f73ffd88bb1c164f9
MarcoSutti <marco.sutt@gmail.com>
2023-09-13 10:19:26 +0800

    updated startup_PrecRTR.m

--- startup_PrecRTR.m
+++ startup_PrecRTR.m
@@ -1,20 +1,18 @@
 %--------------------------------------------------------------------------
 % Startup PrecRTR
 % Created:     2022.08.29
-% Last change: 2022.09.29
+% Last change: 2023.09.13
 %--------------------------------------------------------------------------
 close all; clear; clc;
 
-folder_RMGLS = '../../Marco_Multilevel/matlab/RMGLS-release01/1_RMGLS_EML';
-folder_PrecRTR = '../PrecRTR_matlab';
+folder_PrecRTR = '../PrecRTR';
 
-addpath( genpath(folder_RMGLS) );
 addpath( genpath(folder_PrecRTR) );
 
 % MS, 30.05.2020: The following piece of code is taken over from the manopt
 %                 script 'importmanopt'
 % Ask user if the path should be saved or not
-fprintf('1_RMGLS_EML and PrecRTR were added to Matlab''s path.\n');
+fprintf('PrecRTR was added to Matlab''s path.\n');
 response = input('Save path for future Matlab sessions? [Y/N] ', 's');
 if strcmpi(response, 'Y')
     failed = savepath();
@@ -23,7 +21,7 @@ if strcmpi(response, 'Y')
     else
         fprintf(['Something went wrong.. Perhaps missing permission ' ...
                  'to write on pathdef.m?\nPath not saved: ' ...
-                 'please re-call startup_RMGLS next time.\n']);
+                 'please re-call startup_PrecRTR next time.\n']);
     end
 else
     fprintf('Path not saved: please re-call startup_PrecRTR next time.\n');
